826972dc22abbd104f51b329334e63d1271f8355
David Inga <davidinga@gmail.com>
2019-05-24 14:16:05 -0700

    Added Data Structure: Stack Updated Data Structure: Linked List

---
+++ Swift Algorithms & Data Structures/Data Structures/Stack/Stack.swift
@@ -0,0 +1,31 @@
+//
+//  Stack.swift
+//  Swift Algorithms & Data Structures
+//
+//  Created by David Inga on 5/24/19.
+//  Copyright © 2019 David Inga. All rights reserved.
+//
+
+private struct Stack<Element> {
+    fileprivate var array = [Element]()
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public func peek() -> Element? {
+        return array.last
+    }
+    
+    public mutating func push(_ element: Element) {
+        array.append(element)
+    }
+    
+    public mutating func pop() -> Element? {
+        return array.popLast()
+    }
+}
