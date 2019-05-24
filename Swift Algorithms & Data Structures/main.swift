02a5026617b82d3ef8172dc4fc81629718b445c2
David Inga <davidinga@gmail.com>
2019-05-24 13:40:56 -0700

    Initial Commit

---
+++ Swift Algorithms & Data Structures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Algorithms & Data Structures
+//
+//  Created by David Inga on 5/24/19.
+//  Copyright © 2019 David Inga. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
